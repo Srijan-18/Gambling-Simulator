184416eaa4baf341d0b7de119ea7ddca3fab39bf
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-08 13:36:03 +0530

    Implementation of bet and its result of win or lose

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,6 +1,26 @@
-#!/bin/bash-x
+#!/bin/bash -x
 
 echo "Welcome To Gambling Simulator"
+BET_LOSE=0;
+BET_WIN=1;
 dailyAmount=100;
 betAmount=1;
 
+function betResult()
+{
+	case $1 in
+		$BET_WIN)
+			dailyAmount=$((dailyAmount+betAmount));
+		;;
+		$BET_LOSE)
+			dailyAmount=$((dailyAmount-betAmount));
+		;;
+		*)
+			echo"SOME ERROR"
+		;;
+	esac
+	echo $dailyAmount
+}
+
+betChance=$((RANDOM%2));
+dailyAmount=$(betResult $betChance);
