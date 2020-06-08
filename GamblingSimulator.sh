4c1d2008ddf71df397b7e4f98555663b398a1384
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-08 15:27:54 +0530

    Luckiest and Unluckiest day Display

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -5,7 +5,6 @@ BET_LOSE=0;
 BET_WIN=1;
 declare -A daysWon
 declare -A daysLost
-
 function betResult()
 {
 	case $1 in
@@ -25,6 +24,8 @@ amountWon=0;
 amountLost=0;
 netResult=0;
 winStreakAmount=0;
+largestWinningStreak=0;
+largestLosingStreak=0;
 loseStreakAmount=0;
 for ((dayCount=1; dayCount<=30; dayCount++ ))
 do
@@ -38,16 +39,43 @@ do
 	if((dailyAmount<100))
 	then
 		amountLost=$((amountLost+50));
-		daysLost["Day"$dayCount]=$((loseStreakAmount+50));
+		loseStreakAmount=$((loseStreakAmount+50));
+		daysLost["Day"$dayCount]=$((loseStreakAmount));
+		if((largestLosingStreak<loseStreakAmount))
+		then
+			largestLosingStreak=$loseStreakAmount;
+		fi
 		winStreakAmount=0;
 	else
 		amountWon=$((amountWon+50));
-		daysWon["Day"$dayCount]=$((winStreakAmount+50));
+		winStreakAmount=$((winStreakAmount+50))
+		daysWon["Day"$dayCount]=$((winStreakAmount));
+		if((largestWinningStreak<winStreakAmount))
+		then
+			largestWinningStreak=$winStreakAmount;
+		fi
 		loseStreakAmount=0;
 	fi
 done
 echo "Wining Days are :" ${!daysWon[@]}
 echo "Losing Days are :" ${!daysLost[@]}
+echo "Luckiest Day(s)"
+for key in ${!daysWon[@]}
+do
+	if [ ${daysWon[${key}]} -eq $largestWinningStreak ]
+	then
+		echo $key
+	fi
+done
+echo "Unluckiest Day(s)"
+for key in ${!daysLost[@]}
+do
+	if [ ${daysLost[${key}]} -eq $largestLosingStreak ]
+	then
+		echo $key
+	fi
+done
+
 echo "Amount Won = $amountWon"
 echo "Amount Lost = $amountLost"
 netResult=$((amountWon-amountLost));
